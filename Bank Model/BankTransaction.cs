9caf8d4965a6c8ab9f4cd2eea517604a0b8654e4
Adejumobi Aishat Adedamola <adejumobiaishat2005@gmail.com>
2025-07-30 12:14:25 +0100

    mA bank App

---
+++ Bank Model/BankTransaction.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BankAppAssignment.Bank_Interface;
+
+
+namespace BankAppAssignment.Bank_Model
+{
+    /// <summary>
+    /// Represents a bank transaction.
+    /// </summary>
+    public class BankTransaction
+    {
+        /// <summary>
+        /// Gets or sets the unique identifier for the transaction.
+        /// </summary>
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        /// <summary>
+        /// Gets or sets the type of the transaction (e.g., "Deposit", "Withdrawal", "Transfer").
+        /// </summary>
+        public string Type { get; set; }
+
+        /// <summary>
+        /// Gets or sets the status of the transaction (e.g., "Completed", "Pending", "Failed").
+        /// </summary>
+        public string Status { get; set; }
+
+        /// <summary>
+        /// Gets or sets the date and time when the transaction was created.
+        /// </summary>
+        public DateTime DateCreated { get; set; } = DateTime.Now;
+    }
+}
---
+++ Bank Service/BankJsonRepository.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+
+namespace BankAppAssignment.Bank_Service
+{
+    /// <summary>
+    /// A generic JSON repository for managing bank data.
+    /// </summary>
+    /// <typeparam name="T">The type of the entity to store in the repository.</typeparam>
+    public class BankJsonRepository<T> where T : class
+    {
+        private readonly string _file;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BankJsonRepository{T}"/> class.
+        /// </summary>
+        /// <param name="bankFile">The path to the JSON file where data will be stored.</param>
+        public BankJsonRepository(string bankFile)
+        {
+            _file = bankFile;
+            // If the file does not exist, create it and initialize with an empty JSON array.
+            if (!File.Exists(bankFile))
+            {
+                File.WriteAllText(_file, "[]");
+            }
+        }
+
+        /// <summary>
+        /// Retrieves all entities from the JSON file.
+        /// </summary>
+        /// <returns>A list of all entities of type <typeparamref name="T"/>.</returns>
+        public List<T> GetAll()
+        {
+            string content = File.ReadAllText(_file);
+            return JsonConvert.DeserializeObject<List<T>>(content);
+        }
+
+        /// <summary>
+        /// Saves a list of entities to the JSON file, overwriting existing content.
+        /// </summary>
+        /// <param name="data">The list of entities to save.</param>
+        public void SaveAll(List<T> data)
+        {
+            string content = JsonConvert.SerializeObject(data, Formatting.Indented);
+            File.WriteAllText(_file, JsonConvert.SerializeObject(data, Formatting.Indented));
+        }
+    }
+}
